75b9c22ea8547d81163e9d8956b0c5a05dceeda9
Sonu-H-N <sonuhn0011@gmail.com>
2026-03-16 18:51:56 +0530

    Added Local Storage to Smart Finance Dashboard

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
-let balance = 0;
-let income = 0;
-let expense = 0;
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+function updateLocalStorage(){
+localStorage.setItem("transactions", JSON.stringify(transactions));
+}
 
 function addTransaction(){
 
 const text = document.getElementById("text").value;
 const amount = Number(document.getElementById("amount").value);
 
+if(text === "" || amount === ""){
+alert("Please enter details");
+return;
+}
+
+const transaction = {
+id: Date.now(),
+text: text,
+amount: amount
+};
+
+transactions.push(transaction);
+
+updateLocalStorage();
+showTransactions();
+
+document.getElementById("text").value="";
+document.getElementById("amount").value="";
+}
+
+function showTransactions(){
+
 const list = document.getElementById("list");
+list.innerHTML="";
+
+let income = 0;
+let expense = 0;
+
+transactions.forEach(t => {
 
 const li = document.createElement("li");
 
-li.innerHTML = text + " <span>₹" + amount + "</span>";
+li.innerHTML = `
+${t.text}
+<span>₹${t.amount}</span>
+`;
 
 list.appendChild(li);
 
-if(amount > 0){
-income += amount;
+if(t.amount > 0){
+income += t.amount;
 }else{
-expense += amount;
+expense += t.amount;
 }
 
-balance = income + expense;
+});
+
+const balance = income + expense;
 
 document.getElementById("income").innerText = "₹" + income;
 document.getElementById("expense").innerText = "₹" + Math.abs(expense);
 document.getElementById("balance").innerText = "₹" + balance;
 
-document.getElementById("text").value="";
-document.getElementById("amount").value="";
 }
+
+showTransactions();
