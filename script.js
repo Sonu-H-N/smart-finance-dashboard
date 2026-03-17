afadfc0e1c7e97e482c712852a1d4d917b7162d4
Sonu-H-N <sonuhn0011@gmail.com>
2026-03-17 18:36:32 +0530

    Added finance chart using Chart.js

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+let chart;
+function deleteTransaction(id){
+transactions = transactions.filter(t => t.id !== id);
+updateLocalStorage();
+showTransactions();
+}
+
+function showTransactions(){
+
+const list = document.getElementById("list");
+list.innerHTML="";
+
+let income = 0;
+let expense = 0;
+
+transactions.forEach(t => {
+
+const li = document.createElement("li");
+
+li.classList.add(t.amount > 0 ? "plus" : "minus");
+
+li.innerHTML = `
+${t.text}
+<span>₹${t.amount}</span>
+<button onclick="deleteTransaction(${t.id})">❌</button>
+`;
+
+list.appendChild(li);
+
+if(t.amount > 0){
+income += t.amount;
+}else{
+expense += t.amount;
+}
+
+});
+
+const balance = income + expense;
+
+document.getElementById("income").innerText = "₹" + income;
+document.getElementById("expense").innerText = "₹" + Math.abs(expense);
+document.getElementById("balance").innerText = "₹" + balance;
+
+updateChart(income, Math.abs(expense));
+}
+
+const balance = income + expense;
+
+document.getElementById("income").innerText = "₹" + income;
+document.getElementById("expense").innerText = "₹" + Math.abs(expense);
+document.getElementById("balance").innerText = "₹" + balance;
+
 function deleteTransaction(id){
 transactions = transactions.filter(t => t.id !== id);
 updateLocalStorage();
@@ -82,46 +134,23 @@ document.getElementById("income").innerText = "₹" + income;
 document.getElementById("expense").innerText = "₹" + Math.abs(expense);
 document.getElementById("balance").innerText = "₹" + balance;
 
-}function deleteTransaction(id){
-transactions = transactions.filter(t => t.id !== id);
-updateLocalStorage();
-showTransactions();
+}
+function updateChart(income, expense){
+
+const ctx = document.getElementById("financeChart").getContext("2d");
+
+if(chart){
+chart.destroy();
 }
 
-function showTransactions(){
-
-const list = document.getElementById("list");
-list.innerHTML="";
-
-let income = 0;
-let expense = 0;
-
-transactions.forEach(t => {
-
-const li = document.createElement("li");
-
-li.classList.add(t.amount > 0 ? "plus" : "minus");
-
-li.innerHTML = `
-${t.text}
-<span>₹${t.amount}</span>
-<button onclick="deleteTransaction(${t.id})">❌</button>
-`;
-
-list.appendChild(li);
-
-if(t.amount > 0){
-income += t.amount;
-}else{
-expense += t.amount;
+chart = new Chart(ctx, {
+type: "pie",
+data: {
+labels: ["Income", "Expense"],
+datasets: [{
+data: [income, expense],
+backgroundColor: ["#22c55e", "#ef4444"]
+}]
 }
-
 });
-
-const balance = income + expense;
-
-document.getElementById("income").innerText = "₹" + income;
-document.getElementById("expense").innerText = "₹" + Math.abs(expense);
-document.getElementById("balance").innerText = "₹" + balance;
-
 }
