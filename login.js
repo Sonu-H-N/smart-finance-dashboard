cbdf44440671a8e14f817d9553ca42bc1d32313d
Sonu-H-N <sonuhn0011@gmail.com>
2026-04-26 11:14:01 +0530

    Added basic login system with session handling

--- login.js
+++ login.js
@@ -1,16 +1,13 @@
-function login(){
+window.login = function () {
 
-const username = document.getElementById("username").value;
-const password = document.getElementById("password").value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-// simple demo login
-if(username === "admin" && password === "1234"){
+  if (username === "admin" && password === "1234") {
+    localStorage.setItem("loggedIn", "true");
+    window.location.href = "index.html";
+  } else {
+    document.getElementById("error").innerText = "Invalid credentials";
+  }
 
-localStorage.setItem("loggedIn", "true");
-window.location.href = "index.html";
-
-}else{
-document.getElementById("error").innerText = "Invalid credentials";
-}
-
-}
+};
